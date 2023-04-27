b8956711123d47e4df3d7e22c2becb18eae237d9
Edward <github@edwardcs.com>
2023-04-27 20:23:39 +0300

    Add auth

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware(() => {
+  console.log("Middleware running");
+  return NextResponse.next();
+});
+
+// Stop Middleware running on static files
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
