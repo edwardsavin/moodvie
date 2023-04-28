0b23daab6a3b54c8439516d908abf0e178919ec3
Edward <github@edwardcs.com>
2023-04-29 02:30:46 +0300

    Feat: useSWR to get spotify access token and add react-hot-toast

--- src/env.mjs
+++ src/env.mjs
@@ -7,6 +7,9 @@ import { z } from "zod";
 const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  CLERK_SECRET_KEY: z.string().min(1),
+  SPOTIFY_CLIENT_SECRET: z.string().min(1),
+  SPOTIFY_CLIENT_ID: z.string().min(1),
 });
 
 /**
@@ -14,11 +17,9 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object(
-  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ (
-    {
-      // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
-    }
-  ),
+  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ ({
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  })
 );
 
 /**
@@ -30,7 +31,11 @@ const client = z.object(
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
 };
 
 // Don't touch the part below
@@ -60,7 +65,7 @@ if (!skip) {
   if (parsed.success === false) {
     console.error(
       "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
+      parsed.error.flatten().fieldErrors
     );
     throw new Error("Invalid environment variables");
   }
@@ -74,7 +79,7 @@ if (!skip) {
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
       return target[/** @type {keyof typeof target} */ (prop)];
     },
---
+++ src/pages/api/auth/spotify-access-token.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// Get user's Spotify access token
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const userId = req.query.userId as string;
+
+  try {
+    const response = await fetch(
+      `https://api.clerk.com/v1/users/${userId}/oauth_access_tokens/oauth_spotify`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CLERK_SECRET_KEY as string}`,
+        },
+      }
+    );
+
+    const data = (await response.json()) as Array<{ token: string }>;
+    const token = data?.[0]?.token as string;
+
+    if (token) {
+      res.status(200).json({ token });
+    } else {
+      res.status(404).json({ message: "Token not found" });
+    }
+  } catch (error: unknown) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
