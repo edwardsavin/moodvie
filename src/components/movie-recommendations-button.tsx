dc9df6c749c9e235ff59194521f147e11cc19f60
Edward <github@edwardcs.com>
2023-05-06 02:38:52 +0300

    Feat: call TMDB API with recommended movies

--- src/components/movie-recommendations-button.tsx
+++ src/components/movie-recommendations-button.tsx
@@ -1,34 +1,44 @@
 import { toast } from "react-hot-toast";
 import getMovieRecommendations from "~/utils/get-movie-recommendations";
+import { useState } from "react";
+import MovieRecommendations from "./movie-recommendation";
+
+export type Movie = {
+  title: string;
+  year: string;
+};
 
 // Start fetching movie recommendations based on the user's Spotify history
-const FetchMovieRecommendationsButton = (songs: string) => {
-  const handleClick = () => {
-    const handleAsyncClick = async () => {
-      try {
-        const movieRecommendations = await getMovieRecommendations(songs);
-        console.log(movieRecommendations);
-      } catch (error) {
-        throw error;
-      }
-    };
+const FetchMovieRecommendationsButton = (songs: { songsString: string }) => {
+  const [recommendedMovies, setRecommendedMovies] = useState<Movie[]>([]);
 
-    handleAsyncClick().catch((error) => {
-      if (error instanceof Error) {
-        toast.error(error.message);
-      } else {
-        toast.error("Something went wrong");
-      }
-    });
+  const handleClick = async () => {
+    try {
+      const movies = await getMovieRecommendations(songs.songsString);
+
+      const mappedMovies = movies.map((movie) => {
+        const [title, year] = movie.split(" (");
+        return { title, year: year?.slice(0, -1) };
+      });
+
+      setRecommendedMovies(mappedMovies as Movie[]);
+    } catch (error) {
+      toast.error("Something went wrong, please try again");
+    }
   };
 
   return (
-    <button
-      className="rounded-md bg-white px-4 py-2 text-black"
-      onClick={handleClick}
-    >
-      Get movie recommendations
-    </button>
+    <>
+      <button
+        className="rounded-md bg-white px-4 py-2 text-black"
+        onClick={handleClick as () => void}
+      >
+        Get movie recommendations
+      </button>
+      {recommendedMovies.length > 0 && (
+        <MovieRecommendations movies={recommendedMovies} />
+      )}
+    </>
   );
 };
 
