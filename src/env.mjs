7e736bd0b526b0ba46035e9a007d5f96049b17ac
Edward <github@edwardcs.com>
2023-05-09 04:59:32 +0300

    Chore: add upstash redis

--- src/env.mjs
+++ src/env.mjs
@@ -12,6 +12,8 @@ const server = z.object({
   SPOTIFY_CLIENT_ID: z.string().min(1),
   OPENAI_API_KEY: z.string().min(1),
   TMDB_API_KEY: z.string().min(1),
+  UPSTASH_REDIS_REST_URL: z.string().url(),
+  UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
 });
 
 /**
@@ -40,6 +42,8 @@ const processEnv = {
   SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   TMDB_API_KEY: process.env.TMDB_API_KEY,
+  UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
 };
 
 // Don't touch the part below
