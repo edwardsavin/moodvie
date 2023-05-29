9171c89f593a222f2119da9dcb002b31ba869e9d
Edward <github@edwardcs.com>
2023-05-29 03:37:12 +0300

    Refactor: change signin spotify button to shadcn button

--- src/components/signin-spotify.tsx
+++ src/components/signin-spotify.tsx
@@ -1,11 +1,12 @@
 import { SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const SignInSpotifyButton = () => {
   return (
     <SignInButton mode="modal" redirectUrl="/recommendations">
-      <button className="rounded bg-green-600 px-6 py-3 font-clash_display text-lg font-semibold text-gray-50 transition duration-300 ease-in-out hover:bg-green-700">
+      <Button className="rounded bg-green-600 px-5 py-6 font-clash_display text-lg font-semibold text-gray-50 transition duration-300 ease-in-out hover:bg-green-700">
         Sign in with Spotify
-      </button>
+      </Button>
     </SignInButton>
   );
 };
