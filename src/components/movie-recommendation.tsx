14c45809880fb15b1e15bc4457b49b0b640f0ca7
Edward <github@edwardcs.com>
2023-05-29 06:16:28 +0300

    Refactor: movie modal

--- src/components/movie-recommendation.tsx
+++ src/components/movie-recommendation.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 import useTmdbMovieInfo from "~/utils/hooks/use-tmdb-movie-info";
 import { forwardRef, useEffect, useState } from "react";
 import { api } from "~/utils/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
 import type { MovieInfo } from "~/pages/api/tmdb-fetch-movie-info";
 import type { Movie } from "./movie-recommendations-button";
 
@@ -11,132 +20,25 @@ type MovieRecommendationsProps = {
 };
 
 // Display the movie recommendations modal with a poster, title, release year, overview and links to TMDB and Letterboxd
-export const MovieModal = ({
-  movieInfo,
-  closeModal,
-}: {
-  movieInfo: MovieInfo;
-  closeModal: () => void;
-}) => {
-  const handleOverlayClick = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    closeModal();
-  };
-
-  return (
-    <div className="fixed inset-0 z-10 overflow-y-auto">
-      <div className="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
-        {/* Background overlay */}
-        <div
-          className="fixed inset-0 transition-opacity"
-          aria-hidden="true"
-          onClick={handleOverlayClick}
-        >
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-
-          {/* Modal panel */}
-          <div className="absolute inset-0 transition-opacity">
-            <div className="absolute inset-y-0 right-0 flex max-w-full pl-10">
-              <div className="relative w-screen max-w-md">
-                {/* Modal content */}
-                <div className="transform overflow-hidden rounded-lg bg-gray-800 p-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
-                  <div>
-                    <div className="mt-3 text-center sm:mt-5">
-                      <div className="relative w-full">
-                        <Image
-                          src={`https://image.tmdb.org/t/p/original${
-                            movieInfo.poster_path as string
-                          }`}
-                          alt={movieInfo.title}
-                          width={200}
-                          height={200}
-                          priority
-                        />
-                      </div>
-
-                      <h3
-                        className="text-lg font-medium leading-6 text-white"
-                        id="modal-headline"
-                      >
-                        {movieInfo.title}
-                      </h3>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-300">
-                          {movieInfo.release_date?.split("-")[0]}
-                        </p>
-                        <p className="text-sm text-gray-300">
-                          ⭐
-                          {Math.round((movieInfo.vote_average as number) * 10) /
-                            10}
-                          / 10
-                        </p>
-
-                        <p className="mt-2 text-sm text-gray-300">
-                          {movieInfo.overview}
-                        </p>
-                      </div>
-
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-300">
-                          <a
-                            href={`https://www.themoviedb.org/movie/${movieInfo.id}`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            View on TMDB
-                          </a>
-                        </p>
-
-                        <p className="mt-2 text-sm text-gray-300">
-                          <a
-                            href={`https://letterboxd.com/search/${movieInfo.title}`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            View on Letterboxd
-                          </a>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-// Display the movie recommendations with a poster, title, release year and overview
-export const MovieCard = (movieInfo: MovieInfo) => {
-  const [showModal, setShowModal] = useState(false);
-
+export const MovieModal = ({ movieInfo }: { movieInfo: MovieInfo }) => {
   const imageWidth = 250;
   const imageHeight = 350;
 
-  if (!movieInfo) return <div>Loading...</div>;
-
-  if (movieInfo.message === "No result found") return null;
-
   return (
-    <div
-      className="mt-6 flex transform flex-col gap-1 transition-all duration-200 hover:scale-105"
-      onClick={() => setShowModal(true)}
-    >
-      <div className="mt-4 flex flex-col">
-        <h2 className="font-clash_display font-semibold text-yellow-100">
-          <p className="cursor-pointer text-xl">{movieInfo.title}</p>{" "}
-          <p className="font-archivo">
-            ⭐{Math.round((movieInfo.vote_average as number) * 10) / 10}/10
-          </p>
-        </h2>
-        <p className="text-gray-300">{movieInfo.release_date?.split("-")[0]}</p>
-      </div>
-      <div className="relative w-full">
+    <DialogHeader className="gap-4">
+      <DialogTitle>
+        <div className="font-clash_display text-2xl font-semibold text-yellow-100">
+          {movieInfo.title} ({movieInfo.release_date?.split("-")[0]})
+        </div>
+      </DialogTitle>
+
+      <DialogDescription className="flex flex-col items-center gap-2">
+        <div className="font-clash_display text-lg font-semibold text-yellow-100">
+          ⭐{Math.round((movieInfo.vote_average as number) * 10) / 10}/ 10
+        </div>
+
         <Image
-          className="cursor-pointer rounded-sm shadow-lg hover:animate-pulse hover:shadow-2xl hover:shadow-slate-800/90"
+          className="mb-4 rounded-sm shadow-2xl shadow-slate-800/90"
           src={`https://image.tmdb.org/t/p/original${
             movieInfo.poster_path as string
           }`}
@@ -145,15 +47,83 @@ export const MovieCard = (movieInfo: MovieInfo) => {
           height={imageHeight}
           priority
         />
-      </div>
 
-      {showModal && (
-        <MovieModal
-          movieInfo={movieInfo}
-          closeModal={() => setShowModal(false)}
-        />
-      )}
-    </div>
+        <div className="font-archivo text-base">{movieInfo.overview}</div>
+      </DialogDescription>
+
+      <DialogFooter className="flex flex-col items-center gap-1">
+        <a
+          href={`https://www.themoviedb.org/movie/${movieInfo.id}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            src="/tmdb-alt-short.svg"
+            alt="The Movie Database"
+            width={160}
+            height={160}
+          />
+        </a>
+
+        <a
+          href={`https://letterboxd.com/search/${movieInfo.title}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            src="/letterboxd-horizontal.svg"
+            alt="Letterboxd"
+            width={160}
+            height={160}
+          />
+        </a>
+      </DialogFooter>
+    </DialogHeader>
+  );
+};
+
+// Display the movie recommendations with a poster, title, release year and overview
+export const MovieCard = (movieInfo: MovieInfo) => {
+  const imageWidth = 250;
+  const imageHeight = 350;
+
+  if (!movieInfo) return <div>Loading...</div>;
+
+  if (movieInfo.message === "No result found") return null;
+
+  return (
+    <Dialog>
+      <div className="mt-6 flex transform flex-col gap-1 transition-all duration-200 hover:scale-105">
+        <div className="mt-4 flex flex-col">
+          <h2 className="font-clash_display font-semibold text-yellow-100">
+            <p className="cursor-pointer text-xl">{movieInfo.title}</p>{" "}
+            <p className="font-archivo">
+              ⭐{Math.round((movieInfo.vote_average as number) * 10) / 10}/10
+            </p>
+          </h2>
+          <p className="text-gray-300">
+            {movieInfo.release_date?.split("-")[0]}
+          </p>
+        </div>
+        <div className="relative w-full">
+          <DialogTrigger>
+            <Image
+              className="cursor-pointer rounded-sm shadow-lg hover:animate-pulse hover:shadow-2xl hover:shadow-slate-800/90"
+              src={`https://image.tmdb.org/t/p/original${
+                movieInfo.poster_path as string
+              }`}
+              alt={movieInfo.title}
+              width={imageWidth}
+              height={imageHeight}
+              priority
+            />
+          </DialogTrigger>
+          <DialogContent className="shadow-2xl shadow-slate-900/50">
+            <MovieModal movieInfo={movieInfo} />
+          </DialogContent>
+        </div>
+      </div>
+    </Dialog>
   );
 };
 
