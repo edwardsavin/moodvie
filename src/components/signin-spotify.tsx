e53073b62cd8f79acc747495c662a5913d052008
Edward <github@edwardcs.com>
2023-06-01 06:42:40 +0300

    Feat: add spotify logo to SignInSpotifyButton

--- src/components/signin-spotify.tsx
+++ src/components/signin-spotify.tsx
@@ -1,11 +1,18 @@
 import { SignInButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 const SignInSpotifyButton = () => {
   return (
     <SignInButton mode="modal" redirectUrl="/recommendations">
       <Button className="rounded bg-green-600 px-5 py-6 font-clash_display text-lg font-semibold text-gray-50 transition duration-300 ease-in-out hover:bg-green-700">
-        Sign in with Spotify
+        <span className="mr-1.5">Sign in with Spotify</span>
+        <Image
+          src="/Spotify_Icon_RGB_White.png"
+          alt="Spotify logo"
+          width={21}
+          height={21}
+        />
       </Button>
     </SignInButton>
   );
