bafff3839d25c4b70e3724e7360754d1beb9c8f1
Edward <github@edwardcs.com>
2023-06-01 07:23:50 +0300

    Feat: add Spotify logo to footer

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -1,17 +1,39 @@
 import { Separator } from "./ui/separator";
+import Image from "next/image";
 
 const MainFooter = () => (
   <footer className="mt-auto w-full py-12 text-center">
-    <div className="flex items-center justify-center gap-2">
-      <p className="text-gray-500">Copyright © {new Date().getFullYear()}</p>
-      <Separator orientation="vertical" className="h-4" />
+    <div className="flex flex-col items-center justify-center gap-4">
       <a
-        href="https://edwardcs.com"
-        className="text-xl font-semibold text-green-500 hover:text-green-700"
+        href="https://www.spotify.com"
         target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center justify-center gap-2"
       >
-        Edward Savin
+        <div className="flex items-center justify-center gap-1">
+          <p className="font-clash_display text-sm font-semibold text-gray-300">
+            Powered by
+          </p>
+          <Image
+            src="/Spotify_Logo_RGB_Green.png"
+            alt="Spotify logo"
+            width={80}
+            height={24}
+          />
+        </div>
       </a>
+
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-gray-500">Copyright © {new Date().getFullYear()}</p>
+        <Separator orientation="vertical" className="h-4" />
+        <a
+          href="https://edwardcs.com"
+          className="text-xl font-semibold text-green-500 hover:text-green-700"
+          target="_blank"
+        >
+          Edward Savin
+        </a>
+      </div>
     </div>
   </footer>
 );
